f4c33f1accbf64ea171a82131fedb3601f343c53
Kevin Yook <yook00627@gmail.com>
2018-03-15 23:38:51 +0000

    created header with all the include

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+
+
+#endif
